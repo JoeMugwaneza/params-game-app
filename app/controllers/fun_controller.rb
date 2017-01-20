1060a570952535a0fde2f9008e51b32de8360992
Joseph Mugwaneza <muganezajoseph@gmail.com>
2017-01-20 09:46:26 +0200

    created another page for view guess_number game

--- app/controllers/fun_controller.rb
+++ app/controllers/fun_controller.rb
@@ -1,18 +1,35 @@
 class FunController < ApplicationController
-  def gaming_action
-  	@name = params[:name]
-  	initial_letter= []
-  	initials = @name.upcase.split("")
-  	initial_letter = initials.first
-  	message = ""
+	def gaming_action
+	  	@name = params[:name]
+	  	initial_letter= []
+	  	initials = @name.upcase.split("")
+	  	initial_letter = initials.first
+	  	message = ""
 
-  	if initial_letter == "A"
+	  if initial_letter == "A"
 
-  		@message = "Hey, your name starts with the first letter of the alphabet!"
-  		render "gaming_action.html"
-  	end 
-  end 
+	  		@message = "Hey, your name starts with the first letter of the alphabet!"
+	  		render "gaming_action.html"
+	  end
+	end 
 
-  #def have_fun
-  #end 
-end
+	def have_some_fun
+
+		@number = params[:number]
+		@predicted_number = @number.to_i
+
+
+		if @predicted_number < 35
+    	@screen_view = "Your guess is smaller that the correct number"
+		
+		elsif @predicted_number > 35
+			@screen_view = "Your guess is bigger that the correct number"
+	 
+		elsif @predicted_number == 36
+			 @screen_view =  "Gotta it right!"
+		end
+ 
+	  render "have_some_fun.html"
+
+	end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/gaming_action', to: "fun#gaming_action"
-
+  get '/joe', to: "fun#have_some_fun"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
